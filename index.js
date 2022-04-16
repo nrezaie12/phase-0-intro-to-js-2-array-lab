c85089baa162e8975c8870d990f468e9373f92a0
Neema <nrezaie12@gmail,com>
2022-04-16 04:57:57 -0400

    commit tree 008b758b25f4891deab3dfae43a85e3b5637eb76

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+var cats = ['Milo', 'Otis', 'Garfield'];
+    beforeEach(function () {
+     cats.length = 0;
+
+      cats.push('Milo', 'Otis', 'Garfield');
+    });
+    
+    function destructivelyAppendCat(Ralph){
+      cats.push(Ralph);
+    }
+
+    function destructivelyPrependCat(Bob){
+      cats.unshift(Bob)
+    }
+    function destructivelyRemoveLastCat(){
+      cats.pop()
+    }
+    function destructivelyRemoveFirstCat(){
+      cats.shift()
+    }
+    function appendCat(Broom){
+      var newCat = [...cats, Broom];
+      return newCat
+    }
+    function prependCat(Arnold){
+    var newCat = [Arnold, ...cats];
+      return newCat
+    }
+    function removeLastCat(){
+      var newCat = cats.slice(0, cats.length - 1);
+      return newCat   
+    }
+    function removeFirstCat(){
+      var newCat = cats.slice(1)
+      return newCat
+    }
+    
+    
